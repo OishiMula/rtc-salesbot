a17101f0af779d217a78e6554e609a452faacf8a
OishiMula <oishimula@gmail.com>
2022-04-24 09:44:07 -0400

    logic changes

--- sales_bot.py
+++ sales_bot.py
@@ -14,8 +14,10 @@ import tweepy
 from dotenv import load_dotenv
 from pycoingecko import CoinGeckoAPI
 from ratelimit import limits
+from requests.adapters import HTTPAdapter, Retry
 
 load_dotenv()
+
 logging.basicConfig(
     level=logging.INFO, 
     format='%(asctime)s - %(levelname)s - %(funcName)s - %(message)s',
@@ -37,31 +39,52 @@ twitter = tweepy.API(
     wait_on_rate_limit=True
 )
 
-# Project name and file to store last tweeted information
-project = "Raging Teens Clan"
-last_tweeted_rtc_file = Path('last_sold.dat')
-last_tweeted_furin_file = Path('last_furin_sold.dat')
-
 cg = CoinGeckoAPI()
+
+# Project name and file to store last tweeted information
+PROJECT = "Raging Teens Clan"
+LAST_TWEETED_RTC_FILE = Path('last_sold.dat')
+LAST_TWEETED_FURIN_FILE = Path('last_furin_sold.dat')
 MINUTE = 60
-ada = '₳'
+ADA = '₳'
 
 # The function to retrieve the JSON listings
 def retrieve_sales(p, page_num):
+    opencnft_session = requests.Session()
+    retries = Retry(
+        total = 5,
+        connect = 3,
+        read = 3,
+        backoff_factor = 0.3,
+    )
+    adapter = HTTPAdapter(max_retries=retries)
+    opencnft_session.mount("https://", adapter)
+
     # These endpoints can be changed, either with a .env or hard-code a URL.
     project1_api = f"https://api.opencnft.io/1/policy/{os.getenv('project1')}/transactions?page={page_num}&order=date"
     project2_api = f"https://api.opencnft.io/1/policy/{os.getenv('project2')}/transactions?page={page_num}&order=date"
-    try:
-        match p:
-            case 'rtc':
-                opencnft_response = requests.get(f'{project1_api}')
-            case 'furin':
-                opencnft_response = requests.get(f'{project2_api}')
-        return opencnft_response.json()
-    except requests.exceptions.RequestException as e:
-        logging.error("Endpoint failure - going to sleep.")
-        time.sleep(300)
-        return None
+    
+    while True:
+        try:
+            match p:
+                case 'rtc':
+                    opencnft_response = opencnft_session.get(f'{project1_api}')
+                case 'furin':
+                    opencnft_response = opencnft_session.get(f'{project2_api}')
+            opencnft_response.raise_for_status()
+
+        except requests.exceptions.HTTPError as e:
+            logging.warning(f"{e} - Retrying")
+            time.sleep(5)
+            continue
+
+        except requests.exceptions.RequestException as e:
+            logging.error("Endpoint failure - going to sleep.")
+            time.sleep(300)
+            continue
+
+        break
+    return opencnft_response.json()
 
 # Cycle through the pages on OpenCNFT
 def next_page(p, page_num):
@@ -90,7 +113,7 @@ def compare_listing(project, file):
         # Check the listing downloaded and compare to what was last tweeted
         # If downloaded listing is newer, check the next listing / page
         if int(cnft_listing['items'][num]['sold_at']) > int(last_tweeted['sold_at']):
-            logging.info(f"Listing #{total_listings} - {cnft_listing['items'][num]['unit_name']} is a new sale. Checking next listing.")
+            logging.info(f"Listing #{total_listings} - {cnft_listing['items'][num]['unit_name']} is a new sale.")
             total_listings += 1
             num += 1
             if num == 20:
@@ -121,17 +144,17 @@ def tweet_sale(listing):
     sold_price = int(float(listing['price']) / 1000000)
     asset_mp = listing['marketplace']
     asset_img_raw = listing['thumbnail']['thumbnail'][7:]
-    asset_media_id = retrieve_media_id(asset_img_raw)
 
     # Making exception incase CoinGecko / Twitter are having issues
     while True:
         try:
+            asset_media_id = retrieve_media_id(asset_img_raw)
             usd = cg.get_price(ids='cardano', vs_currencies='usd')
-            logging.info(f"{asset} - Purchased for {ada}{sold_price:,}")
-            twitter.update_status(status=f"{asset} was purchased from {asset_mp} for the price of {ada}{sold_price:,} (${(usd['cardano']['usd'] * sold_price):,.2f}).", media_ids=[asset_media_id.media_id])
+            twitter.update_status(status=f"{asset} was purchased from {asset_mp} for the price of {ADA}{sold_price:,} (${(usd['cardano']['usd'] * sold_price):,.2f}).", media_ids=[asset_media_id.media_id])
+            logging.info(f"{asset} - Purchased for {ADA}{sold_price:,}")
             break
         except (requests.exceptions.RequestException, tweepy.TweepyException) as e:
-            logging.error(e)
+            logging.error(f"{asset} - Error! Retrying...")
             time.sleep(120)
             continue        
 
@@ -151,23 +174,23 @@ def retrieve_media_id(img_raw):
 @limits(calls=30, period=MINUTE)
 def main():
     # Upon starting, it will check for a last_sold files. If none exist, it will enter the most recent sale to begin the monitor.
-    logging.info(f"Starting the {project} Twitter bot.")
-    if last_tweeted_rtc_file.is_file() == False:
-        logging.warning(f"{last_tweeted_rtc_file} not found. Creating file now.")
+    logging.info(f"Starting the {PROJECT} Twitter bot.")
+    if LAST_TWEETED_RTC_FILE.is_file() == False:
+        logging.warning(f"{LAST_TWEETED_RTC_FILE} not found. Creating file now.")
         cnft_listing = retrieve_sales('rtc', 1)
-        pickle.dump(cnft_listing['items'][0], open(last_tweeted_rtc_file, 'wb'))
-    if last_tweeted_furin_file.is_file() == False:
-        logging.warning(f"{last_tweeted_furin_file} not found. Creating file now.")
+        pickle.dump(cnft_listing['items'][0], open(LAST_TWEETED_RTC_FILE, 'wb'))
+    if LAST_TWEETED_FURIN_FILE.is_file() == False:
+        logging.warning(f"{LAST_TWEETED_FURIN_FILE} not found. Creating file now.")
         cnft_listing = retrieve_sales('furin', 1)
-        pickle.dump(cnft_listing['items'][0], open(last_tweeted_furin_file, 'wb'))
+        pickle.dump(cnft_listing['items'][0], open(LAST_TWEETED_FURIN_FILE, 'wb'))
 
     while True:
         raging_teens = 'rtc'
         furins = 'furin'
 
         # The main function to post on twitter if changes are detected
-        compare_listing(raging_teens, last_tweeted_rtc_file)
-        compare_listing(furins, last_tweeted_furin_file)
+        compare_listing(raging_teens, LAST_TWEETED_RTC_FILE)
+        compare_listing(furins, LAST_TWEETED_FURIN_FILE)
         time.sleep(30)
 
 if __name__ == "__main__":
